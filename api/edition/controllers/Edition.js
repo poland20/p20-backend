255af5a9afa64e765d5c0b0ed01c3ab2801cb52f
Artur Komoter <artur@komoter.pl>
2019-06-13 22:53:33 +0100

    Added custom endpoint for current edition data

--- api/edition/controllers/Edition.js
+++ api/edition/controllers/Edition.js
@@ -4,4 +4,6 @@
  * Read the documentation () to implement custom controller functions
  */
 
-module.exports = {};
+module.exports = {
+    currentEdition: () => strapi.services.edition.currentEdition()
+};
--- api/edition/services/Edition.js
+++ api/edition/services/Edition.js
@@ -4,4 +4,13 @@
  * Read the documentation () to implement custom service functions
  */
 
-module.exports = {};
+module.exports = {
+    currentEdition: () => {
+        const populate = Edition.associations
+            .filter(ast => ast.autoPopulate !== false)
+            .map(ast => ast.alias)
+            .join(' ');
+
+        return Edition.findOne().populate(populate);
+    }
+};
