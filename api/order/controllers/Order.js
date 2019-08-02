f37e326b19123292b5f1c5096a771b4ba19161f1
Artur Komoter <artur@komoter.pl>
2019-08-02 21:43:44 +0200

    Pending Order is now created upon creation of a PaymentIntent

--- api/order/controllers/Order.js
+++ api/order/controllers/Order.js
@@ -3,13 +3,33 @@
 const stripe = require('stripe');
 
 module.exports = {
-  paymentIntent: async ctx => {
+  createPaymentIntent: async ctx => {
     const Stripe = stripe(strapi.config.currentEnvironment.stripeApiKey);
-    const { amount } = ctx.request.body;
+    const { amount, basket, participants } = ctx.request.body;
+    if (!amount || !basket || !participants) {
+      ctx.response.badRequest();
+      return;
+    }
+
     const paymentIntent = await Stripe.paymentIntents.create({
       amount: amount * 100,
       currency: 'gbp'
     });
-    ctx.send({ clientSecret: paymentIntent.client_secret });
+
+    // if (err) {
+    //   ctx.response.badImplementation(`Could not create PaymentIntent: ${err}`);
+    //   return;
+    // }
+
+    await strapi.services.order.create({
+      basket,
+      participants,
+      paymentIntent: paymentIntent.id,
+      clientSecret: paymentIntent.client_secret,
+      status: 'pending',
+      date: Date.now()
+    });
+
+    ctx.send({ client_secret: paymentIntent.client_secret });
   }
 };
