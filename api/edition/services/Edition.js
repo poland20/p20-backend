c925eab5aa0fa127f2283d45ff3518c4f7a488c1
Artur Komoter <artur@komoter.pl>
2019-08-10 21:59:52 +0200

    Orders now store the payment amount

--- api/edition/services/Edition.js
+++ api/edition/services/Edition.js
@@ -5,39 +5,39 @@
  */
 
 module.exports = {
-    findOne(params) {
-        let populate = Edition.associations
-            .filter(ast => ast.autoPopulate !== false)
-            .map(ast => ast.alias);
+  findOne(params) {
+    let populate = Edition.associations
+      .filter(ast => ast.autoPopulate !== false)
+      .map(ast => ast.alias);
 
-        populate = populate.concat([
-            {
-                path: 'agendaDays',
-                populate: [
-                {
-                    path: 'venue'
-                },
-                {
-                    path: 'events',
-                    populate: [
-                    {
-                        path: 'speakers'
-                    },
-                    {
-                        path: 'category'
-                    }]
-                }]
-            }
-        ]);
+    populate = populate.concat([
+      {
+        path: 'agendaDays',
+        populate: [
+          {
+            path: 'venue'
+          },
+          {
+            path: 'events',
+            populate: [
+              {
+                path: 'speakers'
+              },
+              {
+                path: 'category'
+              }]
+          }]
+      }
+    ]);
 
-        const edition = Edition.findOne(params).populate(populate);
+    const edition = Edition.findOne(params).populate(populate);
 
-        return edition;
-    },
-    currentEdition(params) {
-        return strapi.services.edition.findOne({ current: true });
-    },
-    findOneByYear(params) {
-        return strapi.services.edition.findOne({ year: params.year });
-    }
+    return edition;
+  },
+  currentEdition(params) {
+    return strapi.services.edition.findOne({ current: true });
+  },
+  findOneByYear(params) {
+    return strapi.services.edition.findOne({ year: params.year });
+  }
 };
